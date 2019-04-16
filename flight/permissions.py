bbf86e731cb753754bce77ff585f643a3d843049
esir <esirkings@gmail.com>
2019-04-16 05:41:25 +0300

    Adds a custom permission to only allow  Admins or owners of a reservation to see and make edits to a Reservation.

--- flight/permissions.py
+++ flight/permissions.py
@@ -10,3 +10,15 @@ class IsGetOrIsAdmin(permissions.BasePermission):
 
         # Otherwise, only allow admin requests
         return request.user and request.user.is_staff
+
+
+class IsAdminOrIsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        """
+        Determines if a user should see or edit a flight reservation
+]       """
+        user = request.user
+        if user:
+            return user.is_staff or obj.passenger == user
+
+        return False
