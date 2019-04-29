c9208f5495a41ef4c7817ee9b145d9863d9ad2b3
esir <esirkings@gmail.com>
2019-04-29 08:46:07 +0300

    Creates a Ticket model to be used for creating tickets.

--- flight/admin.py
+++ flight/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Flight, Reservation
+from .models import Flight, Reservation, Ticket
 
 admin.site.register(Flight)
 admin.site.register(Reservation)
+admin.site.register(Ticket)
